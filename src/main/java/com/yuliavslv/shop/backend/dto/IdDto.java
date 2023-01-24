0c81f0d9b6963a5fbf4fdd3d6dfce4049b729cf1
Yulia Vasileva <vasilyeva.jo@gmail.com>
2023-01-24 16:51:19 +0300

    Added IdDto class

---
+++ src/main/java/com/yuliavslv/shop/backend/dto/IdDto.java
@@ -0,0 +1,16 @@
+package com.yuliavslv.shop.backend.dto;
+
+public class IdDto {
+    private Integer id;
+
+    public IdDto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
