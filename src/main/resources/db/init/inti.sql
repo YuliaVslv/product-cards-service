6d5305f54efb63f5d19503a9e11198064dc3f31b
Yulia Vasileva <vasilyeva.jo@gmail.com>
2023-02-01 13:31:59 +0300

    Changed init.sql to check if tables have already been created

--- src/main/resources/db/init/inti.sql
+++ src/main/resources/db/init/inti.sql
@@ -1,16 +1,16 @@
-CREATE SCHEMA product_cards;
+CREATE SCHEMA IF NOT EXISTS product_cards;
 
-CREATE TABLE product_cards.brand (
+CREATE TABLE IF NOT EXISTS product_cards.brand (
     id INTEGER NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE product_cards.type (
+CREATE TABLE IF NOT EXISTS product_cards.type (
     id INTEGER NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE product_cards.product (
+CREATE TABLE IF NOT EXISTS product_cards.product (
     id INTEGER NOT NULL PRIMARY KEY,
     type_id INTEGER NOT NULL,
     brand_id INTEGER NOT NULL,
@@ -19,5 +19,5 @@ CREATE TABLE product_cards.product (
     discount INTEGER NOT NULL,
     amount INTEGER NOT NULL,
     FOREIGN KEY(type_id) REFERENCES product_cards.type(id),
-    FOREIGN KEY(brand_id) REFERENCES product_cards.brand(id),
+    FOREIGN KEY(brand_id) REFERENCES product_cards.brand(id)
 );
